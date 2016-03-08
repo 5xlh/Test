91cbb46bf07ee9bc198359aeb8ab2a1a52fdadd7
5xlh <marwayhs@hotmail.com>
2016-03-07 20:33:58 -0500

    Python
    Python 3 code

---
+++ script.py
@@ -0,0 +1,47 @@
+"""caesar = "Graham"
+praline = "John"
+viking = "Teresa"
+
+print (caesar)
+print (praline)
+print (viking)
+
+print ('this is\'nt flying it\'s falling with style')
+
+second_letter = "Bhaag" [1]
+print (second_letter) 
+
+elephant = " Helment Cat"
+
+print (len(elephant))
+elephant = "Helment Cat".lower()
+print (elephant)
+elephant= "Helment Cat".upper()
+print (elephant)
+elbow= 2.076
+print (str(elbow))
+
+the_machine_goes = "Ping"
+print (the_machine_goes)
+
+print ("the value of Pi is around "+ str(3+0.14))
+
+# new
+
+string_1 ="Camelot"
+string_2 ="place"
+
+print ("Let's not go to %s. 'Tis a sill %s." %(string_1, string_2))
+
+# new
+
+name = input("What is you name?")
+quest = input ("What is your quest")
+color = input ("What is your favourite color?")
+
+print ("Ah, so your name is %s, your quest is %s,"\
+"and your favourite color is %s." % (name, quest, color)) """
+
+
+
+       
