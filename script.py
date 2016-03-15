aa28dde2ca1d2baac8e54f673d94373e85e2415e
5xlh <marwayhs@hotmail.com>
2016-03-14 21:48:42 -0400

    new code
    added functions and dictionaries

--- script.py
+++ script.py
@@ -225,3 +225,70 @@ print (the_flying_circus()) """
 ##dog  = animals[3:6]              # The fourth through sixth characters
 ##frog = animals[6:10]              # From the seventh character to the end
 ##
+
+"""for loop"""
+##start_list = [5, 3, 1, 2, 4]
+##square_list = []
+##
+### Your code here!
+##for x in start_list:
+##    square_list.append(x**2)
+##    
+##square_list.sort()
+##print (square_list)
+
+""" keys and list """
+##menu = {} # Empty dictionary
+##menu['Chicken Alfredo'] = 14.50 # Adding new key-value pair
+##print (menu['Chicken Alfredo'])
+##
+### Your code here: Add some dish-price pairs to menu!
+##menu['Spam']=2.50
+##menu['Cabbage']=3.60
+##menu['Banana']=1.00
+##
+##
+##
+##print ("There are " + str(len(menu)) + " items on the menu.")
+##print (menu)
+
+""" add and remove to/from a list"""
+### key - animal_name : value - location 
+##zoo_animals = { 'Unicorn' : 'Cotton Candy House',
+##'Sloth' : 'Rainforest Exhibit',
+##'Bengal Tiger' : 'Jungle House',
+##'Atlantic Puffin' : 'Arctic Exhibit',
+##'Rockhopper Penguin' : 'Arctic Exhibit'}
+### A dictionary (or list) declaration may break across multiple lines
+##
+### Removing the 'Unicorn' entry. (Unicorns are incredibly expensive.)
+##del zoo_animals['Unicorn']
+##
+### Your code here!
+##del zoo_animals['Sloth']
+##del zoo_animals['Bengal Tiger']
+##
+##zoo_animals ['Rockhopper Penguin']='Cotton Candy House'
+##
+##print (zoo_animals)
+
+"""list and dictionaries"""
+inventory = {
+    'gold' : 500,
+    'pouch' : ['flint', 'twine', 'gemstone'], # Assigned a new list to 'pouch' key
+    'backpack' : ['xylophone','dagger', 'bedroll','bread loaf']
+}
+
+# Adding a key 'burlap bag' and assigning a list to it
+inventory['burlap bag'] = ['apple', 'small ruby', 'three-toed sloth']
+
+# Sorting the list found under the key 'pouch'
+inventory['pouch'].sort() 
+
+# Your code here
+inventory['pocket']=['seashell','strange berry', 'lint']
+inventory['backpack'].sort()
+inventory['backpack'].remove('dagger')
+inventory['gold']=inventory['gold']+50
+
+print (inventory)
